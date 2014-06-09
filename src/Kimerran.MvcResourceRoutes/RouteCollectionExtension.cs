e8f7924849f2ab7fcc178ed9f871eb6b9ef9ed75
kimerran <mh.neri@gmail.com>
2014-06-09 11:24:25 +0800

    updated conventions and added action name overriding

--- src/Kimerran.MvcResourceRoutes/RouteCollectionExtension.cs
+++ src/Kimerran.MvcResourceRoutes/RouteCollectionExtension.cs
@@ -1,73 +1,121 @@
-﻿using System.Web;
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Kimerran.MvcResourceRoutes
 {
-    public static class RouteCollectionExtension
+    /*
+     * Creates the following routes similar to Laravel / Ruby on Rails conventions 
+     * For example, we have a Books collection
+     * ---------------------------------------------------------------------------
+     * VERB     PATH                CONTROLLER.ACTION           DESCRIPTION
+     * ---------------------------------------------------------------------------
+     * GET      /books              BooksController.Index       
+     * GET      /books/new          BooksController.New
+     * POST     /books              BooksController.Create
+     * GET      /books/{id}         BooksController.Show
+     * GET      /books/{id}/edit    BooksController.Edit
+     * PUT      /books/{id}         BooksController.Update
+     * DELETE   /books/{id}         BooksController.Destroy
+     * 
+     */
+    public static class MvcResourceRoutes
     {
         /// <summary>
-        /// Create routes for a resource similar to Laravel's Resource Controllers
-        /// http://laravel.com/docs/controllers#resource-controllers
+        /// Default Action names. Override using the [SetActionName] method.
+        /// </summary>
+        private static Dictionary<string, string> _routeNames = new Dictionary<string, string>
+        {
+            {"index", "Index"},
+            {"new", "New"},
+            {"create", "Create"},
+            {"show", "Show"},
+            {"edit", "Edit"},
+            {"update", "Update"},
+            {"desctroy", "Destroy"}
+        };
+        
+        /// <summary>
+        /// Create routes for a resource
+        /// Laravel Doc: http://laravel.com/docs/controllers#resource-controllers
+        /// RoR Doc: http://guides.rubyonrails.org/routing.html
         /// </summary>
         /// <param name="routes">The route collection</param>
         /// <param name="resourceName"></param>
+         /// <param name="pathName"></param>
         public static void RegisterResourceRoutes(this RouteCollection routes, string resourceName, string pathName = "")
         {
-            // Example: resourceName = Book
+            // Example: resourceName = Books
 
             if (string.IsNullOrEmpty(pathName)) pathName = resourceName;
 
 
-            // [GET] /book
+            // [GET] /books
             routes.MapRoute(resourceName + "@index",
                 pathName,
-                new { controller = resourceName, action = "Index" },
+                new { controller = resourceName, action = _routeNames["index"]},
                 new { httpMethod = new HttpMethodConstraint("GET") }
             );
 
-            // [GET] /book/create
+            // [GET] /books/create
+            routes.MapRoute(resourceName + "@new",
+                pathName + "/new",
+                new { controller = resourceName, action = _routeNames["new"] },
+                new { httpMethod = new HttpMethodConstraint("GET") }
+            );
+
+            // [POST] /books
             routes.MapRoute(resourceName + "@create",
-                pathName + "/create",
-                new { controller = resourceName, action = "Create" },
-                new { httpMethod = new HttpMethodConstraint("GET") }
-            );
-
-            // [POST] /book
-            routes.MapRoute(resourceName + "@store",
                 pathName,
-                new { controller = resourceName, action = "Store" },
+                new { controller = resourceName, action = _routeNames["create"] },
                 new { httpMethod = new HttpMethodConstraint("POST") }
             );
 
-            // [GET] /book/28/edit
-            routes.MapRoute(resourceName + "@edit",
-                pathName + "/{id}/edit",
-                new { controller = resourceName, action = "Edit" },
-                new { httpMethod = new HttpMethodConstraint("GET") }
-            );
-
-            // [GET] /book/28
+            // [GET] /books/28
             routes.MapRoute(resourceName + "@show",
                 pathName + "/{id}",
-                new { controller = resourceName, action = "Show" },
+                new { controller = resourceName, action = _routeNames["show"] },
                 new { httpMethod = new HttpMethodConstraint("GET") }
             );
 
-            // [PUT] /book/28
+            // [GET] /books/28/edit
+            routes.MapRoute(resourceName + "@edit",
+                pathName + "/{id}/edit",
+                new { controller = resourceName, action = _routeNames["edit"] },
+                new { httpMethod = new HttpMethodConstraint("GET") }
+            );
+
+            // [PUT] /books/28
             routes.MapRoute(resourceName + "@update",
                 pathName + "/{id}",
-                new { controller = resourceName, action = "Update" },
+                new { controller = resourceName, action = _routeNames["update"] },
                 new { httpMethod = new HttpMethodConstraint("PUT") }
             );
 
-            // [DELETE] /book/28
-            routes.MapRoute(resourceName + "@delete",
+            // [DELETE] /books/28
+            routes.MapRoute(resourceName + "@destroy",
                 pathName + "/{id}",
-                new { controller = resourceName, action = "Delete" },
+                new { controller = resourceName, action = _routeNames["destroy"] },
                 new { httpMethod = new HttpMethodConstraint("DELETE") }
             );
         }
+
+        /// <summary>
+        /// Overrides the default action names
+        /// </summary>
+        /// <param name="actionName"></param>
+        /// <param name="newName"></param>
+        public static void SetActionName(string actionName, string newName)
+        {
+            if (!_routeNames.ContainsKey(actionName)) 
+                throw new Exception("Action name should be one of the following: index, new, create, show, edit, update, destroy");
+
+            _routeNames.Remove(actionName);
+            _routeNames.Add(actionName, newName);
+        }
+
 
     }
 }
